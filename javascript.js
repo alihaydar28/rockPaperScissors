f15b0e1832d460853f8f49b7bd45442577f32456
alihaidar <alihaydar282828@gmail.com>
2022-12-27 17:01:13 +0200

    this is commit rps before ui

--- javascript.js
+++ javascript.js
@@ -1,53 +1,77 @@
-//console.log("hello world");
+
 
 function getRandomInt(max) {
     return Math.floor(Math.random() * max);
-  }
+}
 
 function getComputerChoice(){
     choice=["paper","rock","scissors"];
-    //randomNumber = Math.random();
-    //console.log(typeof choice +" "+ typeof choice[0]);
     randomNumber=getRandomInt(3);
-    //console.log(randomNumber);
-    //console.log(choice[randomNumber]);
     return choice[randomNumber];
 }
 
-function game(userSelection , computerSelection){
-
+function playRound(userSelection , computerSelection, userScore, compScore){
+    
     if(userSelection === computerSelection){
         console.log("tie");
     }
     else if(userSelection==="paper"){
         if(computerSelection==="rock"){
-            console.log("user wins");
+            console.log("You wins");
+            userScore++;
         }
         else if(computerSelection==="scissors"){
-            console.log("computer wins wins");
+            console.log("computer wins");
+            compScore++;
         }
     }
     else if(userSelection==="rock"){
         if(computerSelection==="paper"){
             console.log("computer wins");
+            compScore++;
         }
         else if(computerSelection==="scissors"){
-            console.log("user wins");
+            console.log("You wins");
+            userScore++;
         }
     }
     else if(userSelection==="scissors"){
         if(computerSelection==="paper"){
-            console.log("user wins");
+            console.log("You wins");
+            userScore++;
         }
         else if(computerSelection==="rock"){
             console.log("computer wins");
+            compScore++;
         }
+    }
+    console.log("Scores: user:"+userScore+"  || computer:"+compScore);
+    return [userScore, compScore];
+}
+
+function game(){ 
+   
+    let userScore=0;
+    let compScore=0;
+    for(let i=0; i<5;i++){
+        number= String(prompt("enter a choice"));
+        comChoice=getComputerChoice();
+        console.log("round: "+ parseInt(i+1) );
+        console.log("computer choice :"+comChoice);
+        console.log("Your choice : "+number);
+        [userScore, compScore] = playRound(number,comChoice,userScore,compScore);
+        console.log("*********************");
+    }
+    console.log("*********************");
+    console.log("final Score :  user:"+userScore+"  computer:"+compScore+"." );
+    if(userScore>compScore){
+    console.log("winner is user ");
+    }else if(userScore<compScore){
+        console.log("winner is computer");
+    }else{
+        console.log("tie!");
     }
 }
 
+game();
 
-
-comChoice=getComputerChoice();
-console.log("computer choice :"+comChoice);
-console.log("user choice : paper");
-game("paper",comChoice);
