a3d583d172a46fd9885da7fe32743f2e10e832e0
alihaidar <alihaydar282828@gmail.com>
2022-12-28 00:31:42 +0200

    new features with design

--- javascript.js
+++ javascript.js
@@ -10,85 +10,130 @@ function getComputerChoice(){
     return choice[randomNumber];
 }
 
-function playRound(userSelection , computerSelection, userScore, compScore){
-    
+function playRound(userSelection , computerSelection){
+    let nb=0;
     if(userSelection === computerSelection){
         console.log("tie");
+        description.textContent=" it's a TIE !";
     }
     else if(userSelection==="paper"){
         if(computerSelection==="rock"){
             console.log("You wins");
             userScore++;
+            nb=userScoreUI.textContent;
+            nb=parseInt(nb);
+            nb=nb+1;
+            userScoreUI.textContent=nb;
+            description.textContent="paper beats rock, user wins!";
         }
         else if(computerSelection==="scissors"){
             console.log("computer wins");
             compScore++;
+            nb=computerScoreUI.textContent;
+            nb=parseInt(nb);
+            nb=nb+1;
+            computerScoreUI.textContent=nb;
+            description.textContent="scissors beats paper, computer wins!";
         }
     }
     else if(userSelection==="rock"){
         if(computerSelection==="paper"){
             console.log("computer wins");
             compScore++;
+            nb=computerScoreUI.textContent;
+            nb=parseInt(nb);
+            nb=nb+1;
+            computerScoreUI.textContent=nb;
+            description.textContent="paper beats rock, computer wins!";
         }
         else if(computerSelection==="scissors"){
             console.log("You wins");
             userScore++;
+            nb=userScoreUI.textContent;
+            nb=parseInt(nb);
+            nb=nb+1;
+            userScoreUI.textContent=nb;
+            description.textContent="rock beats scissors, user wins!";
         }
     }
     else if(userSelection==="scissors"){
         if(computerSelection==="paper"){
             console.log("You wins");
             userScore++;
+            nb=userScoreUI.textContent;
+            nb=parseInt(nb);
+            nb=nb+1;
+            userScoreUI.textContent=nb;
+            description.textContent="scissors beats paper, user wins!";
         }
         else if(computerSelection==="rock"){
             console.log("computer wins");
             compScore++;
+            nb=computerScoreUI.textContent;
+            nb=parseInt(nb);
+            nb=nb+1;
+            computerScoreUI.textContent=nb;
+            description.textContent="rock beats scissors, computer wins!";
         }
     }
     console.log("Scores: user:"+userScore+"  || computer:"+compScore);
     return [userScore, compScore];
 }
 
-function game(){ 
-   
+
+
+
+    let userChoice1="";
     let userScore=0;
     let compScore=0;
-    /*for(let i=0; i<5;i++){*/
-        //number= String(prompt("enter a choice"));
-        let number="rock";
+    let counter=0;
+    const btnRock = document.querySelector("#rockbtn");
+    const btnSci = document.querySelector(".sci");
+    const btnPaper = document.getElementsByClassName("pap")[0];
+    const userScoreUI=document.getElementById("playerScore");
+    const computerScoreUI=document.getElementById("computerScore");
+    const roundNb=document.getElementById("rnd");
+    const description=document.getElementById("desc");
+
+    btnRock.addEventListener('click', () => {
+        if(counter !=5){
         comChoice=getComputerChoice();
-        console.log("round: "+ parseInt(1) );
-        console.log("computer choice :"+comChoice);
-        console.log("Your choice : "+number);
-        [userScore, compScore] = playRound(number,comChoice,userScore,compScore);
-        console.log("*********************");
-    //}
-    console.log("*********************");
-    console.log("final Score :  user:"+userScore+"  computer:"+compScore+"." );
-    if(userScore>compScore){
-    console.log("winner is user ");
-    }else if(userScore<compScore){
-        console.log("winner is computer");
-    }else{
-        console.log("tie!");
-    }
-}
+        [userScore, compScore] = playRound("rock",comChoice);
+        counter++;
+        roundNb.textContent="Round: "+counter;
+        }else{
+            alert ("game over");
+        }
+    });
 
-//game();
+    
+    btnSci.addEventListener('click', () => {
+        if(counter !=5){
+        comChoice=getComputerChoice();
+        [userScore, compScore] = playRound("scissors",comChoice);
+        counter++;
+        roundNb.textContent="Round: "+counter;
+        }else{
+            alert ("game over");
+        }
+    });
 
-const btnRock = document.querySelector("#rockbtn");
-btnRock.addEventListener('click', () => {
-  alert("rock btn");
-});
+   
+    btnPaper.addEventListener('click', () => {
+        if(counter !=5){
+        comChoice=getComputerChoice();     
+        [userScore, compScore] = playRound("paper",comChoice);
+        counter++;
+        roundNb.textContent="Round: "+counter;
+        }else{
+            alert ("game over");
+        }
+    });
 
-const btnSci = document.querySelector(".sci");
-btnSci.addEventListener('click', () => {
-  alert("scissors btn");
-});
 
-const btnPaper = document.getElementsByClassName("pap")[0];
-btnPaper.addEventListener('click', () => {
-  alert("paper btn");
-});
+
+
+
+
 
 
